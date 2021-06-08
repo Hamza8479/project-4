700d227a724bbbce892aae2c8f01f5431c76b9e4
Hamza <hamzazaman500@gmail.com>
2021-06-08 16:10:56 +0500

    update login

--- frontend/src/Screens/login/Login.jsx
+++ frontend/src/Screens/login/Login.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from 'react';
 import {useHistory } from 'react-router-dom';
-import './login.css'
 function Login() {
     const history=useHistory();
     const [formFields, setformFields] = useState({
@@ -40,12 +39,47 @@ function Login() {
         e.preventDefault();
        checkUserForLogin();
     }
+    const style ={
+        display:"grid",
+        // justifyContent:"center",
+        // alignItems:"center",
+        height:"81vh",
+        width:"100vw",
+        padding:"10px",
+        // gridTemplateRows:"repeact(3,10px)",
+        gridGap:"0px", 
+    backgroundImage: "url(https://store.hp.com/app/assets/images/uploads/prod/25-best-hd-wallpapers-laptops159561982840438.jpg)",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+    };
+    const inpt ={
+        height:"35px",
+        width:"400px",
+        borderTop: "none",
+        borderRight: "none",
+        borderLeft: "none",
+        textAlign: "center",
+        borderRadius:"10px",
+        // background: "transparent",
+        fontSize:"20px",
+        color:"white"
+    }
+    const btn ={
+        background: "rgb(40, 176, 255)",
+        color:"white",
+        width:"100px",
+        height:"35px",
+        marginTop:"10px",
+        borderRadius:"20px",
+        fontSize:"17px",
+    }
     return (
-        <div classname="loginPage">
+        <div style={style} classname="loginPage">
+           <div> <h1 style={{color:"white"}}>Login</h1> </div>
             <form action="">
-               Email <input type="text" onChange={getChange} name="email" value={formFields.email}/> 
-                Password<input type="text" onChange={getChange} name="pass" value={formFields.pass}/>
-                <button onClick={submitData} style={{width:"50px",height:"20px"}}>Login</button>
+              <input style={inpt} type="text" onChange={getChange} name="email" value={formFields.email} placeholder='Email'/>  <br /> <br />
+               <input style={inpt} type="text" onChange={getChange} name="pass" value={formFields.pass} placeholder='Password'/> <br /> 
+                <button style={btn} onClick={submitData}> <b>Login</b> </button>
                 </form>
         </div>
     )
